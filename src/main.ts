f6558f65b0a9b3b3e309ebcdb1d4bcae9cef1ff2
dospore <tetthera@gmail.com>
2023-08-24 17:14:22 +0200

    Update api route

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     .addTag('Beaconchain')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
 
   await app.listen(8000);
 }
